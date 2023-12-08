cd18356b7990c30b4415bf9951fcb23ab0673303
carl10086 <yszlx200@163.com>
2023-12-08 22:40:32 +0800

    feat: init debug env for netty

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,9 +11,15 @@ include("cb-feeds")
 // base 系列
 //include("cb-base:cb-base-scylla")
 
-for (projectName in listOf("scylla","core")) {
+for (projectName in listOf("scylla", "core")) {
     val fullProjectName = "cb-base-${projectName}"
     include(fullProjectName)
     project(":$fullProjectName").projectDir = File("./cb-base/${projectName}")
+}
 
+
+for (projectName in listOf("netty")) {
+    val fullProjectName = "cb-examples-${projectName}"
+    include(fullProjectName)
+    project(":$fullProjectName").projectDir = File("./cb-examples/${projectName}")
 }
